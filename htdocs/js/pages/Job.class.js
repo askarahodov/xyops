a669958076ff84284fdab23ee39bd9a520e1e06e
Joseph Huckaby <jhuckaby@gmail.com>
2024-09-02 22:38:39 -0700

    Make more things clickable in alert table

--- htdocs/js/pages/Job.class.js
+++ htdocs/js/pages/Job.class.js
@@ -550,9 +550,9 @@ Page.Job = class Job extends Page.Base {
 		html += this.getBasicGrid( grid_args, function(item, idx) {
 			return [
 				'<b>' + self.getNiceAlertID(item, true) + '</b>',
-				self.getNiceAlert(item.alert, false),
+				self.getNiceAlert(item.alert, true),
 				item.message,
-				self.getNiceServer(item.server, false),
+				self.getNiceServer(item.server, true),
 				self.getNiceAlertStatus(item),
 				self.getNiceDateTime(item.date),
 				self.getNiceAlertElapsedTime(item, true, true)
